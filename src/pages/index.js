eb65a3f0b18e84f8583b3c779fbeffe76fec13f9
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-08-16 10:06:40 +0700

    add: hmm

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Layout from "@/layouts";
-import { Alert } from "flowbite-react";
 
 const Home = () => {
   return (
     <Layout title="Home">
-      <Alert color="info">Hai</Alert>
-      <h1 className="text-8xl text-pink-600 font-bold">Aku Pulang</h1>
+      <div className="min-h-screen">
+        <h1 className="text-8xl text-pink-600 font-bold">Aku Pulang</h1>
+      </div>
     </Layout>
   );
 };
