674f47c5dfb883b7cd9267240b39b1d163fe105f
fahmikdp <fahmikurnia04@gmail.com>
2022-08-30 21:30:21 +0700

    create Header component

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "@/layouts";
+import { Header } from "@/components";
 
 export default function Services() {
   return (
     <Layout>
-      <h1>Services</h1>
+      <section className="py-10 xl:px-14 md:px-4 sm:px-4 container">
+        <Header title="Services" />
+        {/* <h1>Services</h1> */}
+      </section>
     </Layout>
   );
 }
